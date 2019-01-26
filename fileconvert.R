3b8da0f5f305802fcf3d304aae06617a0dc59921
Faris Naji <fnaji@pamgene.com>
2019-01-26 23:21:18 +0100

    improved code readability

--- fileconvert.R
+++ fileconvert.R
@@ -4,31 +4,35 @@ size_of_file_limit_in_megs = 100
 
 #select file
 
-a_file_name <- file.choose()
+in_file_name <- file.choose()
 
-# orignal filename
-file_path_without_ext <- strsplit(a_file_name, "\\.")[[1]][1]
-file_ext <- strsplit(a_file_name, "\\.")[[1]][2]
-file_basename <- basename(a_file_name)
+# original filename
+get_name_parts <- function(a_file_name) {
+  basename <- basename(a_file_name)
+  path_without_ext <- strsplit(a_file_name, "\\.")[[1]][1]
+  ext <- strsplit(a_file_name, "\\.")[[1]][2]
+  if (ext == "NA") file_ext <-""
+  return(list(basename = basename, path_without_ext = path_without_ext, ext = ext))
+}
 
-print (paste("filename is", file_basename))
+in_file<- get_name_parts(in_file_name)
+
+print (paste("filename is", in_file$basename))
 
 timestamp <- format(Sys.time(), "%y%m%d%H%M")
 
 # new filename
-file_name <- paste0(file_path_without_ext, "-", timestamp, ".", file_ext)
-file_path_without_ext <- strsplit(file_name, "\\.")[[1]][1]
-file_ext <- strsplit(file_name, "\\.")[[1]][2]
-file_basename <- basename(file_name)
-if (file_ext == "NA") file_ext <-""
+file_name <- paste0(in_file$path_without_ext, "-", timestamp, ".", in_file$ext)
 
-print (paste("new filename is", file_basename))
+file_part <-get_name_parts(file_name)
 
+
+print (paste("new filename is", file_name))
 
 print("reading started...")
 print("...it may take some time..")
 print("...please wait...")
-dataset <- read.csv(a_file_name)
+dataset <- read.csv(in_file_name)
 print("reading finished")
 print("conversion started...")
 
@@ -39,40 +43,26 @@ print("added row_nums as an extra column")
 
 
 size_of_input_file_in_bytes <- object.size(dataset)
-size_of_input_file_in_megs <- size_of_input_file_in_bytes / (1024 * 1024)
+size_of_input_file_in_megs  <- size_of_input_file_in_bytes / (1024 * 1024)
 
-print(paste("size of file is",size_of_input_file_in_bytes,"Bytes"))
-
+print(paste("size of file is", size_of_input_file_in_bytes,"Bytes"))
 
 if (size_of_input_file_in_megs > size_of_file_limit_in_megs) {
   print("splitting file in pieces")
   
-  # calculate number of files
+# calculate number of files
   
   num_of_data_points_input_file <- nrow(dataset) * ncol(dataset)
   
   size_per_datapoint_in_bytes = size_of_input_file_in_bytes / num_of_data_points_input_file
-  size_per_datapoint_in_megs  = size_per_datapoint_in_bytes / 10 ^ 6
+  size_per_datapoint_in_megs  = size_per_datapoint_in_bytes / (1024 * 1024)
   
-  num_data_points_per_piece <-
-    size_of_file_limit_in_megs / size_per_datapoint_in_megs
-  num_rows_per_piece <-
-    floor(num_data_points_per_piece / ncol(dataset))
+  num_data_points_per_piece <- size_of_file_limit_in_megs / size_per_datapoint_in_megs
+  num_rows_per_piece        <- floor(num_data_points_per_piece / ncol(dataset))
   num_pieces <- floor((nrow(dataset) / num_rows_per_piece)) + 1
   
   # write pieces
-  print(
-    paste(
-      "splitting",
-      file_basename,
-      "into",
-      num_pieces,
-      "pieces",
-      "each with",
-      num_rows_per_piece,
-      "rows"
-    )
-  )
+  print(paste("splitting", file_part$basename, "into", num_pieces, "pieces", "each with", num_rows_per_piece, "rows"))
   
   for (i in 1:num_pieces) {
     start_index <- (num_rows_per_piece * (i - 1)) + 1
@@ -84,7 +74,7 @@ if (size_of_input_file_in_megs > size_of_file_limit_in_megs) {
     print (paste("piece", i, "rows from",  start_index, "to", end_index))
     
     piece_name <-
-      paste0(file_path_without_ext, "_", i, ".", file_ext)
+      paste0(file_part$path_without_ext, "_", i, ".", file_part$ext)
     print(piece_name)
     
     if (file.exists(piece_name))
